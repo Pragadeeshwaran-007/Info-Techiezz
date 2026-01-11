c25322a71aebd11b6976abb69916a4e2be180c4a
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2026-01-11 19:42:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Info-Techiezz/', // 👈 repo name EXACT
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
